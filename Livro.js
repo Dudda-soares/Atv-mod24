5d5ee01160ea4a0a2af0586e5f4783667e1c178f
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-06-24 20:04:44 -0300

    ok

--- Livro.js
+++ Livro.js
@@ -22,9 +22,21 @@ function LivroDigital(titulo,genero,autor, ano, tamanhoArquivo){
     }
 }
 
+function LivroManga(titulo,genero,autor, ano, edicoes){
+    Livro.call(this, titulo, genero, autor, ano)
+    this.edicoes = edicoes
+
+    this.resumir = function(){
+	    console.log(`O livro ${this.titulo} do gênero ${this.genero} da(o) autor ${this.autor} foi lançado em ${this.ano} e possui atualmente ${this.edicoes} edições lançadas`);
+         
+    }
+}
 
 
-const romance = new Livro("Orgulho e preconceito","romance", "Jane Austin", 2005)
+
+
+
+const romance = new LivroDigital("Orgulho e preconceito","romance", "Jane Austin", 2005, "152 MB")
 romance.resumir();
 
 const politico = new Livro("As Veias Abertas da América Latina", "politico", "Eduardo Galeano", 1971)
@@ -33,5 +45,5 @@ politico.resumir();
 const literatura = new Livro("Dom Casmurro", "literatura", "Machado de Assis", 1899)
 literatura.resumir();
 
-const manga = new LivroDigital("One Piece", "Mangá", "Eiichiro Oda", 1997, "150 MB")
+const manga = new LivroManga("One Piece", "Mangá", "Eiichiro Oda", 1997, 70)
 manga.resumir();
